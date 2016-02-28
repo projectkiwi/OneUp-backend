0098aa42037c53e86126fb4181bd52a6ce7fd64f
Nicky Semenza <14nicholasse@gmail.com>
2016-02-28 15:20:48 -0500

    default view for an asset

--- models/asset.js
+++ models/asset.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var AssetSchema   = new mongoose.Schema({
-  name: String
+  name: String,
+  url: { type: String, default: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150' }
 });
 
 module.exports = mongoose.model('Asset', AssetSchema);
