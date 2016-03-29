c4140a683022f749fcaab8a5274063c38af33f2d
Nicky Semenza <nicky@nickysemenza.com>
2016-03-29 01:42:06 -0400

    update location schema

--- models/location.js
+++ models/location.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose');
 
-var LocationSchema   = new mongoose.Schema({
-  name: String,
-  loc: {
-    type: [Number],  // [<longitude>, <latitude>]
-    index: '2d'      // create the geospatial index
-    },
-    place_id: String, //reference to facebook places API id
-    parent  : { type: mongoose.Schema.ObjectId, ref: 'Location' },
-    type: String
+var LocationSchema = new mongoose.Schema({
+	name: String,
+	place_id: String, //reference to facebook places API id
+	location : {
+		type: { 
+		  type: String,
+		  default: 'Point'
+		}, 
+		coordinates: [Number]
+	}
+
 });
 
+LocationSchema.index({ location : '2dsphere' });
 module.exports = mongoose.model('Location', LocationSchema);
