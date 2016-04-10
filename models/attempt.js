7b27111c5f24b97bcf45395ff06a00b105ce7cc6
Nicky Semenza <nicky@nickysemenza.com>
2016-04-10 17:40:48 -0400

    virtual scheme to generate full url for gif

--- models/attempt.js
+++ models/attempt.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose');
 
+var keys = require('../keys');
+
+var schemaOptions = {
+    toObject: {
+      virtuals: true
+    }
+    ,toJSON: {
+      virtuals: true
+    }
+  };
+
 var AttemptSchema = new mongoose.Schema({
   challenge : { type: mongoose.Schema.ObjectId, ref: 'Challenge' },
   description: { type: String, default: 'Attempt Description' }, 
   user: { type: mongoose.Schema.ObjectId, ref: 'User' },
-  full_img: String,
   gif_img: String,
   orig_video: String,
   comments: [{ type: mongoose.Schema.ObjectId, ref: 'Comment' }],
@@ -12,6 +22,13 @@ var AttemptSchema = new mongoose.Schema({
   like_total: { type: Number, default: 0 },
   created_on: { type: Date, default: Date.now },
   liked_attempt: Boolean
+},schemaOptions);
+
+
+AttemptSchema.virtual('gif_full_url')
+.get(function () {
+  return keys.server_url + "/" + this.gif_img;
 });
+
 
 module.exports = mongoose.model('Attempt', AttemptSchema);
